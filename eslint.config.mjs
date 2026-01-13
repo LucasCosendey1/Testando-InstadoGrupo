e4be0f6aec50a63865b83072929d449810c9aea2
Lucas Cosendey <lukecosendey@gmail.com>
2026-01-13 18:37:55 -0300

    GOZE BATMAN GOZE AGORA, BATMAN HAHAHAHAHAHHAAH

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Suas configurações aqui
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-};
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default nextConfig;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
